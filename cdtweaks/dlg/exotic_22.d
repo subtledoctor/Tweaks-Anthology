477f7ff2e93868001b95155b063a85d799163c81
CamDawg <pcamagna@yahoo.com>
2017-11-19 20:06:32 -0800

    Almost v4

--- cdtweaks/dlg/exotic_22.d
+++
@@ -1,3 +0,0 @@
-REPLACE_ACTION_TEXT _ALVAHE
-  ~StartStore("_TEM4802",LastTalkedToBy(Myself))~
-  ~StartStore("_TEM4003",LastTalkedToBy(Myself))~
--- cdtweaks/dlg/exotic_bgt.d
+++
@@ -1,3 +0,0 @@
-REPLACE_ACTION_TEXT ALVAHE
-  ~StartStore("TEM4802",LastTalkedToBy(Myself))~
-  ~StartStore("TEM4003",LastTalkedToBy(Myself))~
