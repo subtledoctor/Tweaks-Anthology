b56304971dac216340501175c099bdf06362150c
subtledoctor <bm571@nyu.edu>
2020-09-10 15:38:22 -0400

    0.1r
    reversion to official

---
+++ cdtweaks/dlg/ne_korgan.d
@@ -0,0 +1,2 @@
+REPLACE_TRIGGER_TEXT ~korga25p~ ~Alignment("Korgan",CHAOTIC_EVIL)~ ~Alignment("Korgan",MASK_EVIL)~
+REPLACE_TRIGGER_TEXT ~korga25p~ ~Alignment("Korgan",CHAOTIC_GOOD)~ ~!Alignment("Korgan",MASK_EVIL)~
